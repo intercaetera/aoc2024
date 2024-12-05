baf7743f79fc7a180383219eace2e462ac0b634e
intercaetera <author@intercaetera.com>
2024-12-05 19:56:20 +0100

    add day 5 part 2

--- src/aoc/day05.clj
+++ src/aoc/day05.clj
@@ -56,14 +56,49 @@
 (defn middle-element [coll]
   (nth coll (quot (count coll) 2)))
 
+(defn valid-pages? [pages all-rules]
+  (let [rules-for-pages (find-rules-for-pages all-rules pages)]
+    (every? #(satisfies-rule? % pages) rules-for-pages))
+  )
+
 (defn solve [input]
   (let [[all-rules all-pages] (parse input)
         valid-pages (filter (fn [pages]
-                              (let [rules-for-pages (find-rules-for-pages all-rules pages)]
-                                (every? #(satisfies-rule? % pages) rules-for-pages)))
+                              (valid-pages? pages all-rules))
                             all-pages)
         middle-elements (map middle-element valid-pages)]
     (apply + middle-elements)))
 
+(defn swap [v i j]
+  (assoc v j (v i) i (v j)))
+
+(defn find-first-invalid-rule [pages rules]
+  (first (remove #(satisfies-rule? % pages) rules)))
+
+(defn reorder-pages [pages rules]
+  (if (valid-pages? pages rules)
+    pages
+    (let [[x y] (find-first-invalid-rule pages rules)
+          x-idx (.indexOf pages x)
+          y-idx (.indexOf pages y)
+          swapped-pages (swap (vec pages) x-idx y-idx)]
+      (recur swapped-pages rules))))
+
+(defn solve2 [input]
+  (let [[all-rules all-pages] (parse input)
+        invalid-pages (remove (fn [pages]
+                                (valid-pages? pages all-rules))
+                              all-pages)
+        reordered-pages (map
+                          #(reorder-pages %
+                             (find-rules-for-pages
+                               all-rules %))
+                          invalid-pages)
+        middle-elements (map middle-element reordered-pages)]
+    (apply + middle-elements)))
+
 (solve example-input)
 (solve (utils/read-input 5))
+
+(solve2 example-input)
+(solve2 (utils/read-input 5))
