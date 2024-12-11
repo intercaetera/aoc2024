d3c2091f07f73b2a586999eec1310d539ba30c10
intercaetera <author@intercaetera.com>
2024-12-11 21:04:20 +0100

    add day 11 part 2

--- src/aoc/day11.clj
+++ src/aoc/day11.clj
@@ -8,27 +8,32 @@
   (->> (str/split input #"\s+")
        (map parse-long)))
 
-(defn condition-zero [stone] (zero? stone))
-(defn result-zero [_stone] (list 1))
+(defn len [num] (count (str num)))
 
-(defn condition-even [stone]
-    (even? (count (str stone))))
-(defn result-even [stone]
+(defn split-in-half [stone]
   (map (comp parse-long str/join) (partition-all (/ (count (str stone)) 2) (str stone))))
 
-(defn result-other [stone]
-  (list (* stone 2024)))
+(defn safe-add [val count] (if (nil? val) count (+ val count)))
 
-(defn check-stone [stone]
+(defn check-stone [stones [stone count]]
   (cond
-    (condition-zero stone) (result-zero stone)
-    (condition-even stone) (result-even stone)
-    :else (result-other stone)))
+    (zero? stone) (update stones 1 safe-add count)
+    (even? (len stone)) (let [[a b] (split-in-half stone)]
+                          (-> stones
+                              (update a safe-add count)
+                              (update b safe-add count)))
+    :else (update stones (* 2024 stone) safe-add count)))
 
-(defn blink [stones] (mapcat check-stone stones))
+(defn blink [stones] (reduce check-stone {} stones))
 
-(defn solve [input]
-  (count (nth (iterate blink (parse input)) 25)))
+(defn solve-n [n input]
+  (let [starting-stones (parse input)
+        starting-map (reduce (fn [acc stone] (assoc acc stone 1)) {} starting-stones)]
+    (apply + (vals (nth (iterate blink starting-map) n)))))
+
+(def solve (partial solve-n 25))
+(def solve2 (partial solve-n 75))
 
 (solve example-input) ; 55312
 (solve (utils/read-input 11)) ; 239714
+(solve2 (utils/read-input 11)) ; 284973560658514
